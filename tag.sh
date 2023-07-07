b860f19898c434dae995cf6155f42529b981440d
Phil Tyler <phil.tyler@pantheon.io>
2023-07-06 23:12:58 -0700

    copypasta master to main

--- tag.sh
+++ tag.sh
@@ -6,9 +6,9 @@ curl -sL https://git.io/autotag-install | sh --
 # fetch all tags and history:
 git fetch --tags --unshallow --prune
 
-if [ $(git rev-parse --abbrev-ref HEAD) != "master" ]; then
-  # ensure a local 'master' branch exists at 'refs/heads/master'
-  git branch --track master origin/master
+if [ $(git rev-parse --abbrev-ref HEAD) != "main" ]; then
+  # ensure a local 'main' branch exists at 'refs/heads/main'
+  git branch --track main origin/main
 fi
 
 ./bin/autotag -b main
