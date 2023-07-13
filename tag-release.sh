6835ca2ca955566309ef49f93f4193b75450528e
Phil Tyler <phil.tyler@pantheon.io>
2023-07-13 13:39:59 -0700

    on release, create or update v1 branch (#9)

--- tag-release.sh
+++ tag-release.sh
@@ -7,10 +7,24 @@ curl -sL https://git.io/autotag-install | sh --
 git fetch --tags --unshallow --prune
 
 if [ $(git rev-parse --abbrev-ref HEAD) != "main" ]; then
-  # ensure a local 'main' branch exists at 'refs/heads/main'
   git branch --track main origin/main
 fi
 
 NEW_RELEASE=$(./bin/autotag)
 git push --tags
 gh release create "v${NEW_RELEASE}" -t "v${NEW_RELEASE}" --generate-notes
+
+# Extract the major version number
+MAJOR_VERSION=$(echo "${NEW_RELEASE}" | cut -d'.' -f1)
+
+echo "Major version: ${MAJOR_VERSION}"
+
+MAJOR_VERSION_BRANCH="v${MAJOR_VERSION}"
+if git show-ref --verify --quiet "refs/heads/${MAJOR_VERSION_BRANCH}"; then
+  git checkout -b "${MAJOR_VERSION_BRANCH}"
+else
+  git checkout "${MAJOR_VERSION_BRANCH}"
+  git merge --ff-only main
+fi
+
+git push origin "${MAJOR_VERSION_BRANCH}"
